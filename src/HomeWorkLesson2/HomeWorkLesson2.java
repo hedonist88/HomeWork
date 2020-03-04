ab65f1253aac724affcc9736d7e34f054f919954
hedonist88 <roma.zhilyaev@gmail.com>
2020-03-05 02:55:59 +0300

    HomeWork Lesson 2

--- src/HomeWorkLesson2/HomeWorkLesson2.java
+++ src/HomeWorkLesson2/HomeWorkLesson2.java
@@ -1,6 +1,6 @@
 package HomeWorkLesson2;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class HomeWorkLesson2 {
     public static void main(String[] args) {
@@ -57,7 +57,17 @@ public class HomeWorkLesson2 {
             Число 113 не содержится в интервале (25,100)
             Число 72 содержится в интервале (25,100)
         */
-        
+        int leftNum = 25;
+        int rightNum = 100;
+        int randomNum = (int) (5 + (Math.random() * 123));
+        System.out.println("// Задача 4");
+        System.out.println("Сгенерированное число: " + randomNum);
+        if(randomNum > leftNum && randomNum < rightNum) {
+            System.out.println("Число " + randomNum + " входит в диапазон (" + leftNum + ", " + rightNum + ")");
+        } else {
+            System.out.println("Число " + randomNum + " не входит в диапазон (" + leftNum + ", " + rightNum + ")");
+        }
+
         /*
             Задача 5
             Написать код, выводящий на экран случайно сгенерированное трёхзначное число и его наибольшую цифру.
@@ -66,41 +76,112 @@ public class HomeWorkLesson2 {
             В числе 774 наибольшая цифра 7
             В числе 613 наибольшая цифра 6
         */
+        int randomNum2 = (int) (Math.random() * 728);
+        int maxNumOld, maxNumNew, tmpnum2;
+        tmpnum2 = randomNum2;
+        maxNumNew = -1;
+        System.out.println("// Задача 5");
+        while(tmpnum2 != 0){
+            if(maxNumNew == -1){
+                maxNumNew = (tmpnum2 % 10);
+                continue;
+            }
+            maxNumOld = (tmpnum2 % 10); //получаем остаток в десятичной системе
+            if(maxNumOld > maxNumNew) maxNumNew = maxNumOld;
+            tmpnum2 /= 10; //убираем разряд
+        }
+        System.out.println("В числе " + randomNum2 + " наибольшая цифра " + maxNumNew);
 
         /*
             Задача 6
             Создайте программу, выводящую на экран все четырёхзначные числа последовательности 1000 1003 1006 1009 1012 1015 ….
         */
+        System.out.println("// Задача 6");
+        for (int i = 1000; i <= 1015; i = i + 3){ // 9999 для сех чисел, мах для краткости
+            System.out.println(i);
+        }
 
         /*
             Задача 7
             Создайте программу, выводящую на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….
         */
+        System.out.println("// Задача 7");
+        for (int i = 1; i <= 55 * 2; i = i + 2){
+            System.out.println(i);
+        }
 
         /*
             Задача 8
             Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….
         */
+        System.out.println("// Задача 8");
+        for (int i = 90; i >= 0; i = i - 5){
+            System.out.println(i);
+        }
 
         /*
             Задача 9
             Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….
         */
+        System.out.println("// Задача 9");
+        for (int i = 2; i <= Math.pow(2, 20) ; i = i * 2){
+            System.out.println(i);
+        }
 
         /*
             Задача 10
             Создайте программу, вычисляющую факториал натурального числа n, которое пользователь введёт с клавиатуры.
         */
+        int factorial, tmpnum3, j;
+        j = 1;
+        factorial = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("// Задача 10");
+        while (true){
+            System.out.println("Введите целое число (0 для выхода)");
+            int numEnter = in.nextInt();
+            tmpnum3 = numEnter;
+            if(numEnter < 0){
+                System.out.println("Введите число больше нуля");
+            } else {
+                while (tmpnum3 != 0) {
+                    factorial = factorial * j++;
+                    tmpnum3--;
+                }
+                System.out.println("Факториал числа " + numEnter + " равен " + factorial);
+                j = 1;
+                factorial = 1;
+            }
+            if(numEnter == 0) break;
+        }
 
         /*
             Задача 11
             Выведите на экран первые 11 членов последовательности Фибоначчи.
         */
+        int iend = 11;
+        int Fibonacci = 0;
+        int num7 = 1;
+        int num8 = 1;
+        System.out.println("// Задача 11");
+        System.out.println("Первые 11 членов последовательности Фибоначчи:");
+
+        // первые 2 числа "1"
+        System.out.println(num7);
+        System.out.println(num8);
+        for (int i = 3; i <= iend ; i++){
+            Fibonacci = num7 + num8;
+            num7 = num8;
+            num8 = Fibonacci;
+            System.out.println(Fibonacci);
+        }
 
         /*
            Задача 12
            Электронные часы показывают время в формате от 00:00 до 23:59. Подсчитать сколько раз за сутки случается так,
            что слева от двоеточия показывается симметричная комбинация для той, что справа от двоеточия (например, 02:20, 11:11 или 15:51).
         */
+
+        
     }
 }
